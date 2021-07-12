0701b2832c9fd808b0499f34c1718a4e75871c6e
monia <moniatec@gmail.com>
2021-07-12 02:16:40 -0400

    add leetCode 116

---
+++ LeetCode-116.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+
+};
