1688434998fa499a8199d464a422580db4bd2e0e
monia <moniatec@gmail.com>
2021-08-04 11:37:45 -0400

    add leetCode 344

---
+++ LeetCode-344.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/reverse-string/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    let len = s.length
+    for (let i = 0; i < len / 2; i++) {
+        let mid = s[i]
+        s[i] = s[len - i - 1]
+        s[len - i - 1] = mid
+    }
+    return s
+};
